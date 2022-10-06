0e6f94def85a6891afb01d98ca23a96aec144ed0
MaetDol <rndka9@gmail.com>
2022-10-07 04:32:56 +0900

    useLocation 직접 관리하는 상태 사용하게 수정

--- src/@lib/router/hooks/useLocation.ts
+++ src/@lib/router/hooks/useLocation.ts
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+import { BrowserHistoryContext } from '../components/router';
+import { peek } from '../utils/utils';
+
 /**
  * 현재 pathname을 반환합니다
  */
 export function useLocation() {
-  return window.location.pathname;
+  const history = useContext(BrowserHistoryContext);
+
+  return peek(history)?.navigateTo ?? window.location.pathname;
 }
