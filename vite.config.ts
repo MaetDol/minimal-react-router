6a658d03b67073ed500b7e3ef0b6d85071b2a950
MaetDol <rndka9@gmail.com>
2022-10-06 23:41:15 +0900

    router 작업 공간 셋업
    absolute path 를 이용해 깔끔하게 쓸 수 있도록 할꺼에욧

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@router',
+        replacement: resolve(__dirname, './src/lib/router'),
+      },
+    ],
+  },
+});
