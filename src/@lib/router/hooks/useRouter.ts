4c95acc0d7d6bc23f4be496ebf6c9a4ebb959ae0
MaetDol <rndka9@gmail.com>
2022-10-07 04:37:22 +0900

    useRouter 구현

--- src/@lib/router/hooks/useRouter.ts
+++ src/@lib/router/hooks/useRouter.ts
@@ -1,5 +1,12 @@
+import { useCallback } from 'react';
+import { dispatchPushEvent } from '../utils/history';
+
 export function useRouter() {
+  const push = useCallback(<T>(path: string, state?: T) => {
+    dispatchPushEvent(path, state);
+  }, []);
+
   return {
-    push: (path: string) => {},
+    push,
   };
 }
