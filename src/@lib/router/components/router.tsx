9bebac3c5be95ca817af4c68418cb427d3d9bae9
MaetDol <rndka9@gmail.com>
2022-10-07 04:37:33 +0900

    Router 컴포넌트 구현

--- src/@lib/router/components/router.tsx
+++ src/@lib/router/components/router.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import {
+  HistoryState,
+  listenPopStateEvent,
+  listenPushEvent,
+} from '../utils/history';
 
 interface Props extends React.PropsWithChildren {}
 
+const DEFAULT_HISTORY_STATES = [window.history.state];
+export const BrowserHistoryContext = createContext<HistoryState<any>[]>(
+  DEFAULT_HISTORY_STATES
+);
+
 export function Router({ children }: Props) {
-  return <>{children}</>;
+  const [history, setHistory] = useState<HistoryState<any>[]>(
+    DEFAULT_HISTORY_STATES
+  );
+
+  useEffect(() => {
+    const clearPushEventListener = listenPushEvent(setHistory);
+    const clearPopStateEventListener = listenPopStateEvent(setHistory);
+
+    return () => {
+      clearPushEventListener();
+      clearPopStateEventListener();
+    };
+  }, []);
+
+  return (
+    <BrowserHistoryContext.Provider value={history}>
+      {children}
+    </BrowserHistoryContext.Provider>
+  );
 }
